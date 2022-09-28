eb2f5151f9c49e4e063f8af35f3836a3392b514d
Roy Choi <hhchoi@osci.kr>
2022-09-28 15:02:42 +0900

    emotion/react 반영

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -12,5 +15,25 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
-  }
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          ['react-app', { flow: false, typescript: true }],
+          require.resolve('@emotion/babel-preset-css-prop'),
+        ],
+      },
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+    };
+    return config;
+  },
 }
