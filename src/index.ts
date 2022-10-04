776984fdb72a9526949df15a4bfa4f289c319b64
bleuuue <bleuuue@gmail.com>
2022-10-04 11:20:56 +0900

    PCMG-245 translation 원복

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ export { default as Tooltip } from './stories/Tooltip';
 export { default as Clipboard } from './stories/Clipboard';
 export { default as DefinitionList } from './stories/DefinitionList';
 export { default as DragDropZone } from './stories/DragDropZone';
-export { default as t } from './stories/Translation';
 
 export * from './stories/Accordion';
 export * from './stories/AccordionAction';
@@ -34,7 +33,6 @@ export * from './stories/Tooltip';
 export * from './stories/Clipboard';
 export * from './stories/DefinitionList';
 export * from './stories/DragDropZone';
-export * from './stories/Translation';
 
 // Icons
 export * from './stories/icons';
