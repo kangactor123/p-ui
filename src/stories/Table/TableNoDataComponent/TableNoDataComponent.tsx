04e4c3416de95d342e388dfe2c7d1c0208f03eca
kangactor123 <kangactor123@naver.com>
2022-11-29 17:54:53 +0900

    update

--- src/stories/Table/TableNoDataComponent/TableNoDataComponent.tsx
+++ src/stories/Table/TableNoDataComponent/TableNoDataComponent.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { css, SerializedStyles } from '@emotion/react';
 import Button from '../../Button';
+import styled from '@emotion/styled';
 
 export const noDataComponentWithoutButtonByMessage = (message: string): ReactElement => (
   <>
@@ -8,27 +9,10 @@ export const noDataComponentWithoutButtonByMessage = (message: string): ReactEle
   </>
 );
 
-export const nodataStyle = css`
-  margin-top: 16px;
-  border-radius: 4px;
-  font-weight: 500;
-  font-size: 13px;
-  padding-right: 16px;
-  padding-left: 12px;
-  height: 32px;
-  color: #58657f;
-  box-shadow: none;
-  background-color: #dbdee4;
-
-  &:hover {
-    background-color: #ecedf0;
-    box-shadow: none;
-  }
-
-  & .MuiButton-endIcon {
-    color: #58657f;
-    margin-left: 4;
-  }
+const NoDataWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
 `;
 
 export function TableNoDataComponent({
@@ -47,7 +31,7 @@ export function TableNoDataComponent({
   style?: SerializedStyles;
 }): ReactElement {
   return (
-    <>
+    <NoDataWrapper>
       <div css={style}>{message}</div>
       {buttonDisplay && buttonOnClick ? (
         <Button
@@ -56,11 +40,11 @@ export function TableNoDataComponent({
           size="small"
           disabled={buttonDisabled}
           onClick={buttonOnClick}
-          css={nodataStyle}
+          // css={nodataStyle}
         >
           {buttonTitle || ''}
         </Button>
       ) : null}
-    </>
+    </NoDataWrapper>
   );
 }
