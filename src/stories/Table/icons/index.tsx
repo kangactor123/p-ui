3271074ff99c231bed7a145e22991bda592d140b
KangDongHee <kangdonghee@KangDongHeeui-MacBookPro.local>
2023-01-26 09:42:31 +0900

    Show/Hide Column 팝업 디자인

--- src/stories/Table/icons/index.tsx
+++ src/stories/Table/icons/index.tsx
@@ -2,7 +2,7 @@ export { ReactComponent as IconTableUploadClose } from './btn-btn-tb-smclose.svg
 export { ReactComponent as IconTableFilterDefault } from './icon-ic-table-filter.svg';
 // eslint-disable-next-line max-len
 export { ReactComponent as IconTableArrowRightCurrentColor } from './icon-ic-arrow-2-right-current-color.svg';
-export { ReactComponent as IconTableClosePopup } from './ic-close-popup.svg';
+export { ReactComponent as IconTableClosePopup } from './icon-close.svg';
 export { ReactComponent as IconTableSetting } from './icon-ic-table-setting.svg';
 export { ReactComponent as IconTableRefresh } from './ic-table-refresh.svg';
 export { ReactComponent as IconTableCsvDownload } from './btn-btn-icon-btn-table-filter-default.svg';
