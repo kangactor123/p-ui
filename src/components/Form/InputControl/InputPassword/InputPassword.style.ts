05c365c4d9d479d0d57139874428ccbfd5bbab68
nonjee888 <nonjee888@gmail.com>
2023-07-20 15:36:55 +0900

    PCR-6719 inputFile

---
+++ src/components/Form/InputControl/InputPassword/InputPassword.style.ts
@@ -0,0 +1,38 @@
+import { StyledComponent } from '@emotion/styled';
+import {
+  styled as MUIStyled,
+  TextField as MUITextField,
+  TextFieldProps,
+  Theme,
+} from '@mui/material';
+import { MUIStyledCommonProps } from '@mui/system';
+
+export const TextField: StyledComponent<TextFieldProps & MUIStyledCommonProps<Theme>, {}, {}> =
+  MUIStyled(MUITextField)({
+    borderRadius: '4px',
+    color: '#808591',
+    backgroundColor: '#FFFFFF',
+    border: 'unset',
+    maxWidth: '640px',
+    minWidth: '300px',
+    '& .MuiInputBase-multiline': {
+      padding: 0,
+      '& .MuiOutlinedInput-input': {
+        height: '90px !important',
+      },
+    },
+    '& .MuiOutlinedInput-root': {
+      fontSize: '13px',
+      fontWeight: 400,
+      lineHeight: '20px',
+      paddingRight: 'unset',
+      backgroundColor: 'transparent',
+    },
+    '& .Mui-disabled': {
+      backgroundColor: '#E8EAED',
+      color: '#B5B8BF',
+    },
+    '& .Mui-disabled .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#D6D9DE !important',
+    },
+  });
