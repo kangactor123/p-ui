085e774e328bd7a8dd4561efc2fd5db8a3093797
bleuuue <bleuuue@gmail.com>
2023-07-27 17:51:53 +0900

    PCR-6697 checkbox design system 반영

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,20 +1,16 @@
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement } from 'react';
 import { Tooltip as MUITooltip, TooltipProps } from '@mui/material';
-import { PlayceThemeContext, ThemeProvider } from '../../providers';
+import { ThemeProvider } from '../../providers';
 
-interface ITooltipProps extends TooltipProps {
-  title: string | false;
-}
+interface ITooltipProps extends TooltipProps {}
 
 /**
  * @param param title 값이 비어있거나 false 일 경우 툴팁이 나오지 않습니다.
  */
-function Tooltip({ arrow = true, title, ...props }: ITooltipProps): ReactElement {
-  const theme = useContext(PlayceThemeContext);
-  const tooltipTitle = title || '';
+function Tooltip({ arrow = true, title = '', ...props }: ITooltipProps): ReactElement {
   return (
-    <ThemeProvider theme={theme}>
-      <MUITooltip {...props} arrow={arrow} title={tooltipTitle} />
+    <ThemeProvider>
+      <MUITooltip {...props} arrow={arrow} title={title} />
     </ThemeProvider>
   );
 }
